b833f68be16840617509704bbc188c60f729afaf
lucas <lucas.arroyo70@gmail.com>
2023-05-31 12:20:58 +0200

    setup .NET backend project

---
+++ Avalon-API/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Avalon_API.Models;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
