54a1c865bcd95512f5ad4687cde77a9f79a9970c
lucas <lucas.arroyo70@gmail.com>
2023-06-02 18:07:42 +0200

    tout marche enfin

--- Avalon-UI/src/App.vue
+++ Avalon-UI/src/App.vue
@@ -9,8 +9,16 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      <v-btn>
+      <v-btn prepend-icon="mdi-check-circle" append-icon="mdi-account-circle">
+        <template v-slot:prepend>
+          <v-icon color="success"></v-icon>
+        </template>
+
         Button
+
+        <template v-slot:append>
+          <v-icon color="warning"></v-icon>
+        </template>
       </v-btn>
 
       <nav>
--- Avalon-UI/src/main.js
+++ Avalon-UI/src/main.js
@@ -10,6 +10,10 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import "vuetify/dist/vuetify.min.css";
+import '@mdi/font/css/materialdesignicons.css'
+
+
 const vuetify = createVuetify({
   components,
   directives,
