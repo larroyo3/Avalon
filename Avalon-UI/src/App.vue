8ac988dfcfaad0c8e3c9ef31fd88e94052546023
lucas <lucas.arroyo70@gmail.com>
2023-06-02 18:14:01 +0200

    update repo

--- Avalon-UI/src/App.vue
+++ Avalon-UI/src/App.vue
@@ -1,96 +1,65 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  data() {
+    return {
+      login: false,
+
+      responseAvailable: false,
+
+      requestOptions: {
+        method: 'GET',
+        redirect: 'follow'
+      }
+    }
+  },
+
+  methods: {
+    getStarted() {
+      this.login = true
+    },
+
+    fetchAPIData() {
+      fetch("http://localhost:5048/api/TodoItems", this.requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error ::', error));
+    }
+  },
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <v-card theme="dark">
+    <v-layout>
+      <v-navigation-drawer expand-on-hover rail>
+        <v-list v-if="login">
+          <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/34.jpg" title="Sandra Adams"
+            subtitle="sandra_a88@gmailcom">
+          </v-list-item>
+        </v-list>
+        <v-list v-else>
+          <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg">
+            <v-col cols="auto">
+              <v-btn size="small" @click="getStarted" color="orange">Get Started</v-btn>
+            </v-col>
+          </v-list-item>
+        </v-list>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <v-btn prepend-icon="mdi-check-circle" append-icon="mdi-account-circle">
-        <template v-slot:prepend>
-          <v-icon color="success"></v-icon>
-        </template>
+        <v-divider></v-divider>
 
-        Button
+        <v-list density="compact" nav active-color="orange">
+          <v-list-item prepend-icon="mdi-home" to="/" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-plus-box" to="/about" title="Create" value="create"></v-list-item>
+          <v-list-item prepend-icon="mdi-account" title="Account" value="account"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
-        <template v-slot:append>
-          <v-icon color="warning"></v-icon>
-        </template>
-      </v-btn>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+      <RouterView />
+    </v-layout>
+  </v-card>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
